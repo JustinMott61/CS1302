c514a0760f4734c03b8ae52b90416893a91d7997
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-08-21 12:56:40 -0400

    Re-did constants
    I add the constants to the view class because  did the tasks out of order.

--- code/Lab 1/src/edu/westga/cs1302/lab1/view/GetBill.java
+++ code/Lab 1/src/edu/westga/cs1302/lab1/view/GetBill.java
@@ -4,6 +4,8 @@ import edu.westga.cs1302.lab1.model.Bill;
 import edu.westga.cs1302.lab1.model.BillItem;
 
 public class GetBill {
+	private static final double TIP_MULTIPLIER = 0.1;
+	private static final double TAX_MULTIPLIER = 0.2;
 	private Bill bill;
 
 	public GetBill(Bill bill) {
@@ -27,8 +29,8 @@ public class GetBill {
 		
 		text += System.lineSeparator();
 		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
-		double tax = subTotal * 0.2;
-		double tip = subTotal * 0.1;
+		double tax = subTotal * TAX_MULTIPLIER;
+		double tip = subTotal * TIP_MULTIPLIER;
 		text += "TAX - $" + tax + System.lineSeparator();
 		text += "TIP - $" + tip + System.lineSeparator();
 		text += "TOTAL - $" + (subTotal + tip + tax);
