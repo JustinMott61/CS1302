f0e308e248deab59e24d81703cf1e55d06b2ac8c
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-09-10 15:00:01 -0400

    Part 3 of Lab 3 Done
    I added the codebehind to the GUI in the MainWindow class per instruction as well as added the try and catch to the getReciept action.

--- code/Lab 3/src/main/java/edu/westga/cs1302/Lab3/views/MainWindow.java
+++ code/Lab 3/src/main/java/edu/westga/cs1302/Lab3/views/MainWindow.java
@@ -1,7 +1,10 @@
 package edu.westga.cs1302.Lab3.views;
 
+import edu.westga.cs1302.Lab3.model.Bill;
+import edu.westga.cs1302.Lab3.model.BillItem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -12,6 +15,7 @@ import javafx.scene.control.TextField;
  * @version Fall 2025
  */
 public class MainWindow {
+	private Bill bill;
 	@FXML
     private TextField itemAmount;
 
@@ -23,14 +27,24 @@ public class MainWindow {
 
     @FXML
     void getReciept(ActionEvent event) {
-
+    	String name = this.itemName.getText();
+    	double amount = Double.parseDouble(this.itemAmount.getText());
+    	BillView view = new BillView();
+    	try {
+    		BillItem billItem = new BillItem(name, amount);
+    		this.bill.addItem(billItem);
+    	} catch (IllegalArgumentException e){
+    		Alert alert = new Alert(Alert.AlertType.ERROR);
+    		alert.setContentText("Please insert valid item name and amount");
+    		alert.showAndWait();
+    	} 
+    	this.output.setText(view.getText(bill));
     }
-
 	/**
 	 * Perform any needed initialization of UI components and underlying objects.
 	 */
 	public void initialize() {
-
+		this.bill = new Bill();
 	}
 
 }
