b8306d3ab1cac5ec5308dc2ded58288bf8fe2768
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-10-14 13:52:03 -0400

    Added Descending
    I added the  Descending comparator to Lab 6 and tested it as need (May need to update tests depending if everything works as it should)

---
+++ code/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Descending.java
@@ -0,0 +1,56 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import java.util.Comparator;
+
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
+public class Descending implements Comparator<Task> {
+
+	/**
+	 * Compares a List of Tasks based on Descending priority
+	 * 
+	 * @precondition o1 != null && o2 != null
+	 * @postcondition a descending list of tasks based on priority (HIGH,MEDIUM,LOW)
+	 * 
+	 * @return a List in descending order based on priority
+	 */
+	@Override
+	public int compare(Task o1, Task o2) {
+		if ((o1 != null) && (o2 != null)) {
+			if ((o1.getPriority() == TaskPriority.LOW) && (o2.getPriority() == TaskPriority.MEDIUM)) {
+				return 1;
+			}
+			if ((o1.getPriority() == TaskPriority.LOW) && (o2.getPriority() == TaskPriority.HIGH)) {
+				return 1;
+			}
+			if ((o1.getPriority() == TaskPriority.MEDIUM) && (o2.getPriority() == TaskPriority.HIGH)) {
+				return 1;
+			}
+			if (o1.getPriority() == o2.getPriority()){
+				return 0;
+			}
+			if ((o1.getPriority() == TaskPriority.HIGH) && (o2.getPriority() == TaskPriority.LOW)) {
+				return -1;
+			}
+			if ((o1.getPriority() == TaskPriority.HIGH) && (o2.getPriority() == TaskPriority.MEDIUM)) {
+				return -1;
+			}
+			if ((o1.getPriority() == TaskPriority.MEDIUM) && (o2.getPriority() == TaskPriority.LOW)) {
+				return -1;
+			}
+			
+		} else {
+			throw new IllegalArgumentException("List can't contain a null");
+		}
+		return 0;
+	}
+	
+	/**Returns the title Descending instead of package name
+	 *
+	 * @return Descending as package name
+	 */
+	public String toString() {
+		return "Descending";
+	}
+
+}
--- code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/descending/TestCompare.java
+++ code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/descending/TestCompare.java
@@ -4,11 +4,102 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.task_tracker.model.Descending;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
 class TestCompare {
 
+public Descending descending = new Descending();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWhen1TaskIsNull() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			Task task1 = new Task("Important Exam", "Very High priority", TaskPriority.HIGH);
+			Task task2 = null;
+			descending.compare(task1, task2);
+		});
+	}
+
+	@Test
+	void testWhen2TaskIsNull() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			Task task1 = null;
+			Task task2 = null;
+			descending.compare(task1, task2);
+		});
+	}
+	
+	@Test
+	void testWhenBothAreLowPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.LOW);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.LOW);
+		int actual = descending.compare(task1, task2);
+		assertEquals(0, actual);
+	}
+	
+	@Test
+	void testWhenBothAreMediumPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.MEDIUM);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.MEDIUM);
+		int actual = descending.compare(task1, task2);
+		assertEquals(0, actual);
+	}
+	
+	@Test
+	void testWhenBothAreHighPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.HIGH);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.HIGH);
+		int actual = descending.compare(task1, task2);
+		assertEquals(0, actual);
+	}
+	
+	@Test
+	void testWhenOnesLowAndOnesMediumPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.LOW);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.MEDIUM);
+		int actual = descending.compare(task1, task2);
+		assertEquals(1, actual);
+	}
+	
+	@Test
+	void testWhenOnesLowandOnesHighPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.LOW);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.MEDIUM);
+		int actual = descending.compare(task1, task2);
+		assertEquals(1, actual);
+	}
+	
+	@Test
+	void testWhenOnesMediumAndOnesHighPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.MEDIUM);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.HIGH);
+		int actual = descending.compare(task1, task2);
+		assertEquals(1, actual);
+	}
+	
+	@Test
+	void testWhenOnesHighAndOnesLowPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.HIGH);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.LOW);
+		int actual = descending.compare(task1, task2);
+		assertEquals(-1, actual);
+	}
+	
+	@Test
+	void testWhenOnesHighAndOnesMediumPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.HIGH);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.MEDIUM);
+		int actual = descending.compare(task1, task2);
+		assertEquals(-1, actual);
+	}
+	
+	@Test
+	void testWhenOnOnesMediumAndOnesLowPriority() {
+		Task task1 = new Task("Get milk", "ran out today", TaskPriority.MEDIUM);
+		Task task2 = new Task("Schedule change", "no class today", TaskPriority.LOW);
+		int actual = descending.compare(task1, task2);
+		assertEquals(-1, actual);
 	}
 
 }
