b275402a6bb577b6cfdb7a2141dbdd9297feb13b
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-12-04 12:27:57 -0500

    1A complete
    I added all the necessary UI to the mainwindow and made sure everything is properly named.

--- code/Comic Collection/src/main/java/edu/westga/cs1302/comic_collection/views/MainWindow.java
+++ code/Comic Collection/src/main/java/edu/westga/cs1302/comic_collection/views/MainWindow.java
@@ -1,6 +1,10 @@
 package edu.westga.cs1302.comic_collection.views;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 
 
 /** Controller class for MainWindow of the Task Tracker system.
@@ -9,6 +13,22 @@ import javafx.fxml.FXML;
  * @version Fall 2025
  */
 public class MainWindow {
+	
+    @FXML
+    private TextField collectionName;
+
+    @FXML
+    private ListView<?> collections;
+
+    @FXML
+    private MenuItem removeCollection;
+
+    @FXML
+    private Button removeCollectionButton;
+    
+    @FXML
+    private Button addCollectionButton;
+
     
     /** Perform any needed initialization of UI components and underlying objects.
      * 
