f91f05bc756a9f8c7e0825634cd3b764777aedba
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-12-04 14:29:13 -0500

    1D complete
    I bindied everything together and made sure everything worked properly. i added error alerts to mkae sure that the user knew what to do when encountering a mistake.

--- code/Comic Collection/src/main/java/edu/westga/cs1302/comic_collection/views/MainWindow.java
+++ code/Comic Collection/src/main/java/edu/westga/cs1302/comic_collection/views/MainWindow.java
@@ -1,6 +1,10 @@
 package edu.westga.cs1302.comic_collection.views;
 
+import edu.wesga.cs1302.comic_collection.viewmodel.ViewModel;
+import edu.westga.cs1302.comic_collection.model.Collection;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
@@ -13,12 +17,12 @@ import javafx.scene.control.TextField;
  * @version Fall 2025
  */
 public class MainWindow {
-	
+	private ViewModel vm;
     @FXML
     private TextField collectionName;
 
     @FXML
-    private ListView<?> collections;
+    private ListView<Collection> collections;
 
     @FXML
     private MenuItem removeCollection;
@@ -38,6 +42,32 @@ public class MainWindow {
      */
     @FXML
     public void initialize() {
+    	this.vm = new ViewModel();
+    	this.vm.getName().bind(this.collectionName.textProperty());
+    	this.collections.setItems(this.vm.getCollections());
     	
+    	this.removeCollection.setOnAction((Event) -> {
+    		this.vm.removeCollection(this.collections.getSelectionModel().getSelectedItem());
+    	});
+    	
+		this.addCollectionButton.setOnAction((Event) -> {
+			try {
+			this.vm.addCollection();
+			} catch (IllegalArgumentException error) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText(error.getMessage());
+				alert.showAndWait();
+			}
+		});
+
+		this.removeCollectionButton.setOnAction((Event) -> {
+			try {
+			this.vm.removeCollection(this.collections.getSelectionModel().getSelectedItem());
+			} catch (IllegalArgumentException error) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText(error.getMessage());
+				alert.showAndWait();
+			}
+		});
     }
 }
