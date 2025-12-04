5e3b59f88945e485f7ff0f4bdf6b17be337db7cc
JustinMott61 <165024324+JustinMott61@users.noreply.github.com>
2025-12-04 11:35:11 -0500

    Added Project 3
    I changed project 2 to have the proper naming per project 3's instructions and added it to my code folder so that is can be manipulated.

---
+++ code/Comic Collection/src/main/java/edu/westga/cs1302/comic_collection/views/MainWindow.java
@@ -0,0 +1,23 @@
+package edu.westga.cs1302.comic_collection.views;
+
+import javafx.fxml.FXML;
+
+
+/** Controller class for MainWindow of the Task Tracker system.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class MainWindow {
+    
+    /** Perform any needed initialization of UI components and underlying objects.
+     * 
+     * @precondition none
+     * @postcondition none
+     * 
+     */
+    @FXML
+    public void initialize() {
+    	
+    }
+}
